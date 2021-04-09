fe9b26461c693d09fa52294d3c2f4be142e60ce1
marko <marko.seidenglanz@gmail.com>
2021-04-09 17:39:45 +0200

    calibration / measurement / switching via boot button integrated

--- src/main.c
+++ src/main.c
@@ -1,142 +1,189 @@
 #include "mgos.h"
-#include "mgos_mqtt.h"
-#ifdef MGOS_HAVE_WIFI
-#include "mgos_wifi.h"
-#endif
+#include "mgos_dht.h"
+#include "mgos_adc.h"
+#include "pt/pt.h"
 
-static void timer_cb(void *arg) {
-  static bool s_tick_tock = false;
-  LOG(LL_INFO,
-      ("%s uptime: %.2lf, RAM: %lu, %lu free", (s_tick_tock ? "Tick" : "Tock"),
-       mgos_uptime(), (unsigned long) mgos_get_heap_size(),
-       (unsigned long) mgos_get_free_heap_size()));
-  s_tick_tock = !s_tick_tock;
-  (void) arg;
-}
+static uint8_t operation_mode = 0; // 0 - Messung | 1 - Calibrierung
+static struct mgos_dht *s_dht = NULL;
+static unsigned long global_millis = 0;
+static uint8_t adc_attenuation = 3; // ADC Dämpfungsfaktor
+static uint8_t temperature = 1;
+static uint8_t humidity = 1;
+static double r0 = 1;
+static uint32_t ppm = 0;
 
-static void net_cb(int ev, void *evd, void *arg) {
-  switch (ev) {
-    case MGOS_NET_EV_DISCONNECTED:
-      LOG(LL_INFO, ("%s", "Net disconnected"));
-      break;
-    case MGOS_NET_EV_CONNECTING:
-      LOG(LL_INFO, ("%s", "Net connecting..."));
-      break;
-    case MGOS_NET_EV_CONNECTED:
-      LOG(LL_INFO, ("%s", "Net connected"));
-      break;
-    case MGOS_NET_EV_IP_ACQUIRED:
-      LOG(LL_INFO, ("%s", "Net got IP address"));
-      break;
+// threads
+static struct pt pt_dht, pt_mq135, pt_button;
+
+int dht_thread(struct pt *pt, uint16_t ticks)
+{
+  static uint16_t timer;
+  timer += ticks;
+
+  PT_BEGIN(pt);
+
+  while (1)
+  {
+    float t = mgos_dht_get_temp(s_dht);
+    float h = mgos_dht_get_humidity(s_dht);
+
+    if (!isnan(h) && !isnan(t))
+    {
+      temperature = t;
+      humidity = h;
+      //LOG(LL_INFO, ("temperature: %f *C humidity: %f %%\n", t, h));
+    }
+
+    // sleep 1 second
+    timer = 0;
+    PT_WAIT_UNTIL(pt, timer >= 1000);
   }
 
-  (void) evd;
-  (void) arg;
+  PT_END(pt);
 }
 
-#ifdef MGOS_HAVE_WIFI
-static void wifi_cb(int ev, void *evd, void *arg) {
-  switch (ev) {
-    case MGOS_WIFI_EV_STA_DISCONNECTED: {
-      struct mgos_wifi_sta_disconnected_arg *da =
-          (struct mgos_wifi_sta_disconnected_arg *) evd;
-      LOG(LL_INFO, ("WiFi STA disconnected, reason %d", da->reason));
-      break;
-    }
-    case MGOS_WIFI_EV_STA_CONNECTING:
-      LOG(LL_INFO, ("WiFi STA connecting %p", arg));
-      break;
-    case MGOS_WIFI_EV_STA_CONNECTED:
-      LOG(LL_INFO, ("WiFi STA connected %p", arg));
-      break;
-    case MGOS_WIFI_EV_STA_IP_ACQUIRED:
-      LOG(LL_INFO, ("WiFi STA IP acquired %p", arg));
-      break;
-    case MGOS_WIFI_EV_AP_STA_CONNECTED: {
-      struct mgos_wifi_ap_sta_connected_arg *aa =
-          (struct mgos_wifi_ap_sta_connected_arg *) evd;
-      LOG(LL_INFO, ("WiFi AP STA connected MAC %02x:%02x:%02x:%02x:%02x:%02x",
-                    aa->mac[0], aa->mac[1], aa->mac[2], aa->mac[3], aa->mac[4],
-                    aa->mac[5]));
-      break;
-    }
-    case MGOS_WIFI_EV_AP_STA_DISCONNECTED: {
-      struct mgos_wifi_ap_sta_disconnected_arg *aa =
-          (struct mgos_wifi_ap_sta_disconnected_arg *) evd;
-      LOG(LL_INFO,
-          ("WiFi AP STA disconnected MAC %02x:%02x:%02x:%02x:%02x:%02x",
-           aa->mac[0], aa->mac[1], aa->mac[2], aa->mac[3], aa->mac[4],
-           aa->mac[5]));
-      break;
-    }
-  }
-  (void) arg;
-}
-#endif /* MGOS_HAVE_WIFI */
+static float a = 121.4517;
+static float b = -2.78054;
+int mq135_thread(struct pt *pt, uint16_t ticks)
+{
+  static uint16_t timer;
+  timer += ticks;
 
-static void cloud_cb(int ev, void *evd, void *arg) {
-  struct mgos_cloud_arg *ca = (struct mgos_cloud_arg *) evd;
-  switch (ev) {
-    case MGOS_EVENT_CLOUD_CONNECTED: {
-      LOG(LL_INFO, ("Cloud connected (%d)", ca->type));
-      break;
+  PT_BEGIN(pt);
+
+  while (1)
+  {
+    uint8_t i = 0;
+    int measurements[10] = {};
+    while (i < 10)
+    {
+      int val = mgos_adc_read(mgos_sys_config_get_mq135_pin());
+      // ggf. ADC Dämpfungsfaktor anpassen
+      if (val < 1000 || val > 3000)
+      {
+        if (val < 1000 && adc_attenuation)
+          adc_attenuation--;
+        else if (val > 3000 && adc_attenuation < 3)
+          adc_attenuation++;
+
+        LOG(LL_INFO, ("attenuation: %d measured val: \n", adc_attenuation, val));
+        esp32_set_channel_attenuation(mgos_sys_config_get_mq135_pin(), adc_attenuation);
+        mgos_adc_enable(mgos_sys_config_get_mq135_pin());
+        continue;
+      }
+
+      measurements[i++] = val;
+    };
+    int sum = 0;
+    while (i--)
+      sum += measurements[i];
+    int meanVal = sum / 10;
+
+    float rs = (4095.0 / meanVal - 1);
+
+    //LOG(LL_INFO, ("sum: %d\n", sum));
+    //LOG(LL_INFO, ("mean: %d\n", meanVal));
+    //LOG(LL_INFO, ("rs: %f\n", rs));
+
+    if (operation_mode == 0)
+    {
+      float rs_scaling_factor = (1.6979 - 0.012 * temperature - 0.00612 * humidity);
+      float rs_corrected = rs / rs_scaling_factor;
+      ppm = a * pow((rs_corrected / r0), b);
+      LOG(LL_INFO, ("temperature: %d *C humidity: %d %% attenuation: %d ppm: %d\n", temperature, humidity, adc_attenuation, ppm));
     }
-    case MGOS_EVENT_CLOUD_DISCONNECTED: {
-      LOG(LL_INFO, ("Cloud disconnected (%d)", ca->type));
-      break;
+    else if (operation_mode == 1)
+    {
+      r0 = rs * pow((a / 400.0), (1.0 / b));
+      LOG(LL_INFO, ("temperature: %d *C humidity: %d %% attenuation: %d r0: %f\n", temperature, humidity, adc_attenuation, r0));
     }
-  }
 
-  (void) arg;
-}
-
-static void button_cb(int pin, void *arg) {
-  char topic[100];
-  snprintf(topic, sizeof(topic), "/devices/%s/events",
-           mgos_sys_config_get_device_id());
-  bool res = mgos_mqtt_pubf(topic, 0, false /* retain */,
-                            "{total_ram: %lu, free_ram: %lu}",
-                            (unsigned long) mgos_get_heap_size(),
-                            (unsigned long) mgos_get_free_heap_size());
-  char buf[8];
-  LOG(LL_INFO,
-      ("Pin: %s, published: %s", mgos_gpio_str(pin, buf), res ? "yes" : "no"));
-  (void) arg;
-}
-
-enum mgos_app_init_result mgos_app_init(void) {
-  char buf[8];
-
-  /* Simple repeating timer */
-  mgos_set_timer(1000, MGOS_TIMER_REPEAT, timer_cb, NULL);
-
-  /* Publish to MQTT on button press */
-  int btn_pin = mgos_sys_config_get_board_btn1_pin();
-  if (btn_pin >= 0) {
-    enum mgos_gpio_pull_type btn_pull;
-    enum mgos_gpio_int_mode btn_int_edge;
-    if (mgos_sys_config_get_board_btn1_pull_up()) {
-      btn_pull = MGOS_GPIO_PULL_UP;
-      btn_int_edge = MGOS_GPIO_INT_EDGE_NEG;
-    } else {
-      btn_pull = MGOS_GPIO_PULL_DOWN;
-      btn_int_edge = MGOS_GPIO_INT_EDGE_POS;
-    }
-    LOG(LL_INFO, ("Button pin %s, active %s", mgos_gpio_str(btn_pin, buf),
-                  (mgos_sys_config_get_board_btn1_pull_up() ? "low" : "high")));
-    mgos_gpio_set_button_handler(btn_pin, btn_pull, btn_int_edge, 20, button_cb,
-                                 NULL);
+    // sleep 1 second
+    timer = 0;
+    PT_WAIT_UNTIL(pt, timer >= 1000);
   }
 
-  /* Network connectivity events */
-  mgos_event_add_group_handler(MGOS_EVENT_GRP_NET, net_cb, NULL);
+  PT_END(pt);
+}
 
-#ifdef MGOS_HAVE_WIFI
-  mgos_event_add_group_handler(MGOS_WIFI_EV_BASE, wifi_cb, NULL);
-#endif
+int button_thread(struct pt *pt, uint16_t ticks)
+{
 
-  mgos_event_add_handler(MGOS_EVENT_CLOUD_CONNECTED, cloud_cb, NULL);
-  mgos_event_add_handler(MGOS_EVENT_CLOUD_DISCONNECTED, cloud_cb, NULL);
+  static uint16_t timer;
+  timer += ticks;
+
+  PT_BEGIN(pt);
+
+  while (1)
+  {
+    // wait until button was pressed
+    PT_WAIT_UNTIL(pt, !mgos_gpio_read(mgos_sys_config_get_button_pin()));
+
+    // wait until button was released
+    PT_WAIT_UNTIL(pt, mgos_gpio_read(mgos_sys_config_get_button_pin()));
+
+    // switch operation mode
+    operation_mode = (operation_mode + 1) % 2;
+    if (operation_mode)
+      LOG(LL_INFO, ("***** CALIBRATE *****\n"));
+    else
+      LOG(LL_INFO, ("***** MEASURE *****\n"));
+  }
+
+  PT_END(pt);
+}
+
+static void main_loop(void *arg)
+{
+  while (1)
+  {
+    unsigned long ts = mgos_uptime_micros() / 1000;
+    unsigned long ticks = ts - global_millis;
+    global_millis = ts;
+
+    // start threads
+    dht_thread(&pt_dht, ticks);
+    mq135_thread(&pt_mq135, ticks);
+    button_thread(&pt_button, ticks);
+
+    // feed watchdog
+    mgos_wdt_feed();
+  }
+}
+
+/*
+static void on_boot_button_pressed(int pin, void *arg)
+{
+  LOG(LL_INFO, ("***** BUTTON PRESSED %d\n",pin));
+  if (pin == mgos_sys_config_get_button_pin())
+  {
+    //LOG(LL_INFO, ("***** BUTTON PRESSED\r\n"));
+  }
+}
+*/
+
+enum mgos_app_init_result mgos_app_init(void)
+{
+  // init dht
+  if ((s_dht = mgos_dht_create(mgos_sys_config_get_dht_pin(), DHT11)) == NULL)
+  {
+    return MGOS_APP_INIT_ERROR;
+  }
+
+  // init mq135
+  esp32_set_channel_attenuation(mgos_sys_config_get_mq135_pin(), adc_attenuation);
+  mgos_adc_enable(mgos_sys_config_get_mq135_pin());
+
+  // init boot button
+  //mgos_gpio_set_button_handler(mgos_sys_config_get_button_pin(), MGOS_GPIO_PULL_UP, MGOS_GPIO_INT_EDGE_NEG, 50, on_boot_button_pressed, NULL);
+
+  // init threads
+  PT_INIT(&pt_dht);
+  PT_INIT(&pt_mq135);
+  PT_INIT(&pt_button);
+
+  // start main loop
+  mgos_set_timer(0, false, main_loop, NULL);
 
   return MGOS_APP_INIT_SUCCESS;
 }
